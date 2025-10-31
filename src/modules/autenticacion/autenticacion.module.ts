5016260692c80632a5b9cc7b724c0ec231ab6ace
Santy_M <santy15021502@gmail.com>
2025-10-31 17:33:49 -0300

    mini cambio

--- src/modules/autenticacion/autenticacion.module.ts
+++ src/modules/autenticacion/autenticacion.module.ts
@@ -1,19 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AutenticacionService } from './autenticacion.service';
 import { AutenticacionController } from './autenticacion.controller';
-import { JwtModule } from '@nestjs/jwt';
+import { AutenticacionService } from './autenticacion.service';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 
 @Module({
-  imports: [
-    UsuariosModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '1h' },
-    }),
-  ],
+  imports: [UsuariosModule],
   controllers: [AutenticacionController],
   providers: [AutenticacionService],
-  exports: [],
 })
 export class AutenticacionModule {}
