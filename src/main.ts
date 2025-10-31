a074216192be2e04afe19a58f4be98ce11d54608
Santy_M <santy15021502@gmail.com>
2025-10-31 16:07:17 -0300

    mini cambio

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,26 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
+import * as serverless from 'serverless-http';
+
 dotenv.config();
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors();
-  await app.listen(3000);
+let cachedServer: any = null;
+
+async function bootstrapServer() {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    app.enableCors();
+    await app.init(); // importante, no app.listen()
+    cachedServer = app.getHttpAdapter().getInstance();
+  }
+  return cachedServer;
 }
-bootstrap();
+
+// handler exportado para Lambda
+export const handler = async (event: any, context: any) => {
+  const server = await bootstrapServer();
+  const proxy = serverless(server);
+  return proxy(event, context);
+};
