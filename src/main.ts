97e6bbf9eb276c76ff80020eff68837c392a1045
Santy_M <santy15021502@gmail.com>
2025-10-31 16:10:35 -0300

    mini cambio

--- src/main.ts
+++ src/main.ts
@@ -13,13 +13,13 @@ async function bootstrapServer() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
     app.enableCors();
-    await app.init(); // importante, no app.listen()
+    await app.init(); // inicializa Nest, no app.listen()
     cachedServer = app.getHttpAdapter().getInstance();
   }
   return cachedServer;
 }
 
-// handler exportado para Lambda
+// export named, Lambda espera un handler
 export const handler = async (event: any, context: any) => {
   const server = await bootstrapServer();
   const proxy = serverless(server);
