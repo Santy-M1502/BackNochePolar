2a074665f73f9f8afa8d3c78959c75c180cad518
Santy_M <santy15021502@gmail.com>
2025-11-04 11:28:27 -0300

    mini cambio otro

--- src/modules/autenticacion/autenticacion.service.ts
+++ src/modules/autenticacion/autenticacion.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, BadRequestException } from '@nestjs/common';
 import { UsuariosService } from '../usuarios/usuarios.service';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
@@ -8,24 +8,24 @@ export class AutenticacionService {
   constructor(
     private usersService: UsuariosService,
     private jwtService: JwtService,
-  ) { }
+  ) {}
 
   async signIn(email: string, pass: string): Promise<{ access_token: string }> {
-    console.log('acaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, ', email, pass)
     const user = await this.usersService.findByEmail(email);
 
-    if(!user){
-      throw new Error("No existe una cuenta con ese email");
-      
+    if (!user) {
+      throw new BadRequestException('No existe una cuenta con ese email');
     }
 
-    if (!(await bcrypt.compare(pass, user.claveHash))) {
-      throw new UnauthorizedException('Credenciales inválidas');
+    const passwordCorrecta = await bcrypt.compare(pass, user.claveHash);
+
+    if (!passwordCorrecta) {
+      throw new UnauthorizedException('Contraseña incorrecta');
     }
 
     const payload = {
       sub: user._id.toString(),
-      username: user.username
+      username: user.username,
     };
 
     return {
@@ -35,10 +35,11 @@ export class AutenticacionService {
 
   async getUpdatedProfile(userId: string) {
     const user = await this.usersService.findById(userId);
+
     if (!user) {
-      throw new UnauthorizedException('Usuario no encontrado');
+      throw new BadRequestException('Usuario no encontrado');
     }
-    
+
     const { claveHash, ...result } = user.toObject();
     return result;
   }
