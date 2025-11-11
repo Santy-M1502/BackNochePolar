ac1e4edd26b054f98583b53e44d6e2db31625be2
Santy_M <santy15021502@gmail.com>
2025-11-11 20:31:32 -0300

    minicambio

--- src/modules/chat/chat.gateway.ts
+++ src/modules/chat/chat.gateway.ts
@@ -2,19 +2,17 @@ import {
   WebSocketGateway,
   WebSocketServer,
   SubscribeMessage,
-  UseGuards,
   MessageBody,
   ConnectedSocket
 } from '@nestjs/websockets';
+import { UseGuards } from '@nestjs/common';
 import { Server } from 'socket.io';
 import { WsAuthGuard } from '../autenticacion/authenticacion-ws.guard';
 import { ChatService } from './chat.service';
 
 @WebSocketGateway({ cors: true })
-@UseGuards(WsAuthGuard)
 export class ChatGateway {
   @WebSocketServer() server: Server;
-
   private clients = new Map<string, string>();
 
   constructor(private chatService: ChatService) {}
@@ -24,13 +22,13 @@ export class ChatGateway {
     this.clients.set(user._id, client.id);
   }
 
+  @UseGuards(WsAuthGuard)
   @SubscribeMessage('send_message')
   async handleMessage(
     @ConnectedSocket() client: any,
     @MessageBody() { to, text }: { to: string; text: string }
   ) {
     const from = client.data.user._id;
-
     await this.chatService.sendMessage(from, to, text);
 
     const targetSocketId = this.clients.get(to);
